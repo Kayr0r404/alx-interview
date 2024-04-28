41e7f5a032c627115082e56fde83152e21d7af4a
Kayr0r404 <mahlarekarabo.702@gmail.com>
2024-04-28 05:23:53 +0200

    locked

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,29 +1,30 @@
 #!/usr/bin/python3
-
+"""locked boxes"""
 
 from collections import deque
 
+
 def canUnlockAll(boxes):
-    '''Checks if all the boxes in a list of boxes containing the keys
+    """Checks if all the boxes in a list of boxes containing the keys
     (indices) to other boxes can be unlocked given that the first
     box is unlocked.
-    '''
+    """
     if not boxes or len(boxes) == 0:
         return False
-    
+
     n = len(boxes)
     visited = [False] * n
     visited[0] = True  # Mark the first box (index 0) as visited
     queue = deque([0])  # Start BFS traversal from the first box
-    
+
     while queue:
         current_box = queue.popleft()
         keys = boxes[current_box]
-        
+
         for key in keys:
             if 0 <= key < n and not visited[key]:
                 visited[key] = True
                 queue.append(key)
-    
+
     # Check if all boxes have been visited
     return all(visited)
