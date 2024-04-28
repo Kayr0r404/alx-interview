f6b4f27443e55b72b00adf32437bda42a2b8a298
Karabo Mahlare <123119832+Kayr0r404@users.noreply.github.com>
2024-04-28 05:19:45 +0200

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,15 +1,25 @@
 #!/usr/bin/python3
 
 
+from collections import deque
+
 def canUnlockAll(boxes):
-    keys = set(())
-    for box in boxes:
-        for key in box:
-            keys.add(key)
-    for key in keys:
-        pass
-    return keys
-
-
-boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-print(canUnlockAll(boxes))
+    if not boxes or len(boxes) == 0:
+        return False
+    
+    n = len(boxes)
+    visited = [False] * n
+    visited[0] = True  # Mark the first box (index 0) as visited
+    queue = deque([0])  # Start BFS traversal from the first box
+    
+    while queue:
+        current_box = queue.popleft()
+        keys = boxes[current_box]
+        
+        for key in keys:
+            if 0 <= key < n and not visited[key]:
+                visited[key] = True
+                queue.append(key)
+    
+    # Check if all boxes have been visited
+    return all(visited)
