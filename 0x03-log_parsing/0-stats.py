9e571a28083356b7394f62b11963e7ac068a9b48
Karabo Mahlare <123119832+Kayr0r404@users.noreply.github.com>
2024-05-02 21:46:07 +0200

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,88 +1,71 @@
 #!/usr/bin/python3
-'''A script for parsing HTTP request logs.
-'''
+"""Log parsing"""
+
+
+import sys
 import re
 
 
-def extract_input(input_line):
-    '''Extracts sections of a line of an HTTP request log.
-    '''
-    fp = (
-        r'\s*(?P<ip>\S+)\s*',
-        r'\s*\[(?P<date>\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+)\]',
-        r'\s*"(?P<request>[^"]*)"\s*',
-        r'\s*(?P<status_code>\S+)',
-        r'\s*(?P<file_size>\d+)'
+def verify_log_entry(log_entry):
+    """verify the stdin format"""
+    # Define the regex pattern to match the specified format
+    pattern = (
+        r"[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+ - \[[0-9]+-[0-9]+-[0-9]+ "
+        r'[0-9]+:[0-9]+:[0-9]+\.[0-9]+\] "GET /projects/260 HTTP/1\.1" '
+        r"([0-9]+) ([0-9]+)"
     )
-    info = {
-        'status_code': 0,
-        'file_size': 0,
+    # Use re.match to check if the log entry matches the pattern
+    match = re.match(pattern, log_entry)
+    if match:
+        return True
+    return False
+
+
+def output_format(file_size, dictionary) -> None:
+    """print dictionary"""
+    print("File size: {}".format(file_size), flush=True)
+    for key, val in sorted(dictionary.items()):
+        if val == 0:
+            continue
+        print("{:s}: {:d}".format(key, val), flush=True)
+
+
+def dict_of_status_code():
+    """Returns possible status code"""
+    return {
+        "200": 0,
+        "301": 0,
+        "400": 0,
+        "401": 0,
+        "403": 0,
+        "404": 0,
+        "405": 0,
+        "500": 0,
     }
-    log_fmt = '{}\\-{}{}{}{}\\s*'.format(fp[0], fp[1], fp[2], fp[3], fp[4])
-    resp_match = re.fullmatch(log_fmt, input_line)
-    if resp_match is not None:
-        status_code = resp_match.group('status_code')
-        file_size = int(resp_match.group('file_size'))
-        info['status_code'] = status_code
-        info['file_size'] = file_size
-    return info
 
 
-def print_statistics(total_file_size, status_codes_stats):
-    '''Prints the accumulated statistics of the HTTP request log.
-    '''
-    print('File size: {:d}'.format(total_file_size), flush=True)
-    for status_code in sorted(status_codes_stats.keys()):
-        num = status_codes_stats.get(status_code, 0)
-        if num > 0:
-            print('{:s}: {:d}'.format(status_code, num), flush=True)
+def run_stats_computation():
+    """reads stdin line by line and computes metrics:"""
+    counter, file_size = 1, 0
 
-
-def update_metrics(line, total_file_size, status_codes_stats):
-    '''Updates the metrics from a given HTTP request log.
-
-    Args:
-        line (str): The line of input from which to retrieve the metrics.
-
-    Returns:
-        int: The new total file size.
-    '''
-    line_info = extract_input(line)
-    status_code = line_info.get('status_code', '0')
-    if status_code in status_codes_stats.keys():
-        status_codes_stats[status_code] += 1
-    return total_file_size + line_info['file_size']
-
-
-def run():
-    '''Starts the log parser.
-    '''
-    line_num = 0
-    total_file_size = 0
-    status_codes_stats = {
-        '200': 0,
-        '301': 0,
-        '400': 0,
-        '401': 0,
-        '403': 0,
-        '404': 0,
-        '405': 0,
-        '500': 0,
-    }
+    dict_of_code = dict_of_status_code()
+    print()
     try:
-        while True:
-            line = input()
-            total_file_size = update_metrics(
-                line,
-                total_file_size,
-                status_codes_stats,
-            )
-            line_num += 1
-            if line_num % 10 == 0:
-                print_statistics(total_file_size, status_codes_stats)
+        for line in sys.stdin:
+
+            if verify_log_entry(line):
+                if counter == 11:
+                    counter = 0
+                    output_format(file_size, dict_of_code)
+                    dict_of_code = dict_of_status_code()
+                counter += 1
+                file_size += int(line.rstrip().split()[-1])
+                status_code = line.rstrip().split()[-2]
+                if status_code in dict_of_code.keys():
+                    dict_of_code[status_code] += 1
     except (KeyboardInterrupt, EOFError):
-        print_statistics(total_file_size, status_codes_stats)
+        output_format(file_size, dict_of_code)
 
 
-if __name__ == '__main__':
-    run()
+if __name__ == "__main__":
+    run_stats_computation()
