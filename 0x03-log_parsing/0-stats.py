e8bebd2faee47a7b8cdbde6d7d6c08aa9e16357d
Kayr0r404 <mahlarekarabo.702@gmail.com>
2024-05-03 05:14:34 +0200

    log

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -7,7 +7,7 @@ import re
 
 
 def verify_log_entry(log_entry):
-    """verify the stdin format"""
+    """Verify if the log entry matches the specified format"""
     # Define the regex pattern to match the specified format
     pattern = (
         r"[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+ - \[[0-9]+-[0-9]+-[0-9]+ "
@@ -17,22 +17,33 @@ def verify_log_entry(log_entry):
     # Use re.match to check if the log entry matches the pattern
     match = re.match(pattern, log_entry)
     if match:
-        return True
-    return False
+        file_size = int(log_entry.rstrip().split()[-1])
+        status_code = log_entry.rstrip().split()[-2]
+    return {"status_code": status_code, "file_size": file_size}
 
 
-def output_format(file_size, dictionary) -> None:
-    """print dictionary"""
-    print("File size: {}".format(file_size), flush=True)
-    for key, val in sorted(dictionary.items()):
-        if val == 0:
-            continue
-        print("{:s}: {:d}".format(key, val), flush=True)
+def output_format(total_file_size, status_codes_stats):
+    """Print the computed metrics"""
+    print("File size: {:d}".format(total_file_size), flush=True)
+    for key, val in sorted(status_codes_stats.items()):
+        if val > 0:
+            print("{:s}: {:d}".format(key, val), flush=True)
 
 
-def dict_of_status_code():
-    """Returns possible status code"""
-    return {
+def update_metrics(line, total_file_size, status_codes_stats):
+    """Updates the metrics from a given HTTP request log."""
+    line_info = verify_log_entry(line)
+    status_code = line_info.get("status_code", "0")
+    if status_code in status_codes_stats.keys():
+        status_codes_stats[status_code] += 1
+    return total_file_size + line_info["file_size"]
+
+
+def run_stats_computations():
+    """Starts the log parser."""
+    line_num = 0
+    total_file_size = 0
+    status_codes_stats = {
         "200": 0,
         "301": 0,
         "400": 0,
@@ -42,32 +53,20 @@ def dict_of_status_code():
         "405": 0,
         "500": 0,
     }
-
-
-def run_stats_computation():
-    """reads stdin line by line and computes metrics:"""
-    counter, file_size = 0, 0
-
-    dict_of_code = dict_of_status_code()
     try:
         while True:
             line = input()
-            if verify_log_entry(line):
-
-                file_size += int(line.rstrip().split()[-1])
-                status_code = line.rstrip().split()[-2]
-
-                if status_code in dict_of_code.keys() and isinstance(
-                    int(status_code), int
-                ):
-                    dict_of_code[status_code] += 1
-            counter += 1
-            if counter % 10 == 0:
-                output_format(file_size, dict_of_code)
-                dict_of_code = dict_of_status_code()
+            total_file_size = update_metrics(
+                line,
+                total_file_size,
+                status_codes_stats,
+            )
+            line_num += 1
+            if line_num % 10 == 0:
+                output_format(total_file_size, status_codes_stats)
     except (KeyboardInterrupt, EOFError):
-        output_format(file_size, dict_of_code)
+        output_format(total_file_size, status_codes_stats)
 
 
 if __name__ == "__main__":
-    run_stats_computation()
+    run_stats_computations()
